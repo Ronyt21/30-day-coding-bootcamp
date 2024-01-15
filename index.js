cf1f2e5ebee67a4d29d4d76fecd829acf7a2646b
Ronit Raj <83657606+Ronyt21@users.noreply.github.com>
2024-01-15 14:32:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-window.addEventListener('keydown', function(e){
-    // console.log(e.keyCode);
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); 
-    if(!audio) return;
+// Declarations
+const key = document.querySelectorAll('.key');
 
-    audio.play();
-    key.classList.add('playing');
-    
-    
-    
-});
+// Addition of transition function to each Keys 
+key.forEach(key=>{
+    key.addEventListener('transitionend', removeTransition)
+})
+
+// Events
+window.addEventListener('keydown', (e) => playMusic(e);
+
+// Function Declarations
+function playMusic(event){
+     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); 
+     if(!audio) return;
+     audio.play();
+     key.classList.add('playing');
+}
 
 function removeTransition(e){
     if(e.propertyName !== 'transform') return;
-
     this.classList.remove('playing');
     console.log(this);
 }
 
-const key = document.querySelectorAll('.key');
-key.forEach(key=>{
-    key.addEventListener('transitionend', removeTransition)
-})
+
+
 
 
